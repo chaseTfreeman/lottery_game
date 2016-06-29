4c48019ece358739d2ad07c19c4e0bea1a571bfa
Chase <chasefreeman13@gmail.com>
2016-06-28 21:41:12 -0400

    Got Basic Lotto Running

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("Hello World");
+
+
+var myName="Chase";
+var lottoWinners = ["Brandon", "Lauren", "Chase"];
+
+for (var i=0; i > lottoWinners.length; i++) {
+    console.log(lottoWinners);
+}
+var randomName = Math.floor(Math.random()*lottoWinners.length);
+var winner = lottoWinners[randomName];
+
+if (myName != winner) {
+
+   console.log("Try Again Next Time!");
+}
+else {
+    console.log("You won!");
+}
